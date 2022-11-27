5450e5a1e1bd9555fb03b453d03ff579fad27642
DiogoSant0s <up202009291@up.pt>
2022-11-27 18:29:36 +0000

    Samples, not working

---
+++ samples.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int main(int argc, char* argv[]) {
+  FILE* fp = fopen(argv[1], "r");
+  int n = atoi(argv[2]);
+  int m = atoi(argv[3]);
+  srandom(0);
+
+  if (argc != 4) {
+  printf("usage: samples filename numberfrags maxfragsize\n");
+  return EXIT_FAILURE;
+  }
+
+  struct stat s;
+  int file_size = stat(argv[1], &s);
+  if (file_size == -1) {
+    puts("There was an error getting file info");
+    exit(-1);
+  }
+
+
+  for (int i = 0; i < n; i++) {
+    fseek(fp, random()%(file_size - m), SEEK_SET);
+    printf(">");
+    for (int j = 0; j < m; j++) {
+      printf("%c", fgetc(fp));
+    }
+    printf("<\n");
+  }
+  fclose(fp);
+
+  return EXIT_SUCCESS;
+}
