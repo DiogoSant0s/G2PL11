15eb417a6f6886e7c722ec9337442ad69eb9881d
Rafael Valquaresma <113989853+Valquaresma03@users.noreply.github.com>
2022-12-01 23:00:30 +0000

    Update txt2epub.c

--- Q2/txt2epub.c
+++ Q2/txt2epub.c
@@ -5,23 +5,19 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-void pandoc_converter(char *fname) {
-    char *command = "pandoc";
-    char *setting = "-o";
-    char *setting1 = "--quiet"; //no warnings
-    char *arg1 = fname;
-    //file output
-    char *output_fname = strdup(fname);
+void Filename_converter(char *filename_txt) {
+    char *arg1 = filename_txt;
+    char *output_fname = strdup(filename_txt); //filename.epub
     int len = strlen(arg1);
     // tirar .txt e pôr .epub
-    output_fname[len] = 'b';
-    output_fname[len - 1] = 'u';
-    output_fname[len - 2] = 'p';
-    output_fname[len - 3] = 'e';
-    output_fname[len - 4] = '.';
+    output_filename[len] = 'b';
+    output_filename[len - 1] = 'u';
+    output_filename[len - 2] = 'p';
+    output_filename[len - 3] = 'e';
+    output_filename[len - 4] = '.';
 
-    //o execlp executa o comando que lhe passo em primeiro lugar com o argv que lhe passo no resto dos argumentos
-    execlp(command, command, fname, setting, output_fname, setting1, (char *)0);
+    //executa o pandoc mudando o valor do filename.txt para o filename.epub
+    execlp((char *)"pandoc", (char *)"pandoc",(char *)filename_txt, (char *)"-o", (char *)output_filename, "--quiet", (char *)0);
 }
 
 int main(int argc, char *argv[]) {
@@ -35,27 +31,23 @@ int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; i++) {
         int pid;  // para saber se é o pai ou o filho
         
-        // fork processo
         pid = fork();
-        if ((pid) < 0) {
-            // Se o pid for menor que 0 então dá erro
+        if ((pid) < 0) { 
+            //fork error se pid menor que 0
             fprintf(stderr, "Fork failed");
             exit(EXIT_FAILURE);
     	} else if (pid == 0) {
-    	    // se o pid for 0 então este é o processo filho
-            pandoc_converter(fname);
+    	    // processo filho
+            pandoc_converter(argv[i]);
     	} else {
-    	    // se o pid for maior que 0, então este é o processo pai
+    	    // processo pai
         	// esperar que o processo filho acabe
-    	    printf("[pid%i] converting %s ... \n", getpid(), fname);
+    	    printf("[pid%i] converting %s ... \n", getpid(), argv[i]);
     	}
     }
 
     // esperar pelos filhos
      for (int i = 1; i < argc; i++) {
-    /*
-        int status = 0;
-        pid_t childpid = wait(&status);*/
         wait(0);
     }
     
